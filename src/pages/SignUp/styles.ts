602aa66cc41a999a0ce9bb23e58a2a5f747336e7
joaohcca <joaohccalbu@gmail.com>
2021-03-09 21:52:04 -0300

    signUp, unform e default ze

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackground from '../../assets/sign-in-background.png';
+import signUpBackground from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -25,7 +25,7 @@ export const Content = styled.div`
   }
 
   form {
-    margin: 80px 0;
+    margin: 70px 0;
     width: 340px;
     text-align: center;
 
@@ -47,7 +47,7 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #ff9000;
+    color: #f4ede8;
     display: block;
     margin-top: -48px;
     text-decoration: none;
@@ -61,13 +61,13 @@ export const Content = styled.div`
     }
 
     &:hover {
-      color: ${shade(0.2, '#ff9000')};
+      color: ${shade(0.2, '#f4ede8')};
     }
   }
 `;
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${signInBackground}) no-repeat center;
+  background: url(${signUpBackground}) no-repeat center;
   background-size: cover;
 `;
