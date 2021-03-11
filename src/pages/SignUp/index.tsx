baee7bc0f2bffe91ead2a13f1511806a483a1fcd
joaohcca <joaohccalbu@gmail.com>
2021-03-10 22:08:18 -0300

    add error tooltips and general validation rules

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,15 +13,19 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   // eslint-disable-next-line @typescript-eslint/ban-types
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
-        name: Yup.string().required('Nome Obrigatório'),
+        name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
-          .required('Email Obrigatório')
-          .email('Digite um e-mail válido'),
-        passwod: Yup.string().min(8, 'Mínimo 8 dígitos'),
+          .email('E-mail inválido')
+          .required('E-mail obrigatório'),
+        password: Yup.string().min(6, 'Mínimo 6 dígitos'),
       });
 
       await schema.validate(data, {
@@ -27,6 +33,11 @@ const SignUp: React.FC = () => {
       });
     } catch (err) {
       console.log(err);
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      }
     }
   }, []);
 
@@ -36,7 +47,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit} initialData={{ name: 'Zé' }}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
