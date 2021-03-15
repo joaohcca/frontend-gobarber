3a9095102780519bd0249fcb8454c14ac7dd7bde
joaohcca <joaohccalbu@gmail.com>
2021-03-15 08:38:15 -0300

    adding authcontext

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { classicNameResolver } from 'typescript';
 
 import { Container } from './styles';
 
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -22,8 +22,9 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { user, signIn } = useContext(AuthContext);
 
+  console.log(user);
   const handleSubmit = useCallback(
     // eslint-disable-next-line @typescript-eslint/ban-types
     async (data: SignInFormData) => {
@@ -46,7 +47,6 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (err) {
-        console.log(err);
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
 
