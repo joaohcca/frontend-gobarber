a0d99afe545db53f3410476bb855a77b22a7e359
joaohcca <joaohccalbu@gmail.com>
2021-03-22 22:47:19 -0300

    add routes and transictions effects to pages

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signUpBackground from '../../assets/sign-up-background.png';
@@ -15,7 +15,7 @@ export const Content = styled.div`
   flex-direction: column;
 
   align-items: center;
-  place-content: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
@@ -23,9 +23,29 @@ export const Content = styled.div`
   img {
     margin-bottom: -30px;
   }
+`;
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
-    margin: 70px 0;
+    margin: 80px 0;
     width: 340px;
     text-align: center;
 
@@ -61,7 +81,7 @@ export const Content = styled.div`
     }
 
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
   }
 `;
